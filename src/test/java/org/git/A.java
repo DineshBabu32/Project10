7e40e81fbd6ab339a400bb5d33b64428ef43b92f
DineshBabu32 <postbox0032@gmail.com>
2025-02-22 22:40:23 +0530

    Update A.java

--- src/test/java/org/git/A.java
+++ src/test/java/org/git/A.java
@@ -2,10 +2,10 @@ package org.git;
 
 public class A {
 
+
 	public void methodA() {
 		System.out.println("A");
 	}
-
 	public void methodB() {
 		System.out.println("B");
 	}
@@ -14,12 +14,8 @@ public class A {
 		System.out.println("C");
 	}
 
-	public void methodD() {
-		System.out.println("D");
-	}
-	public void methodE() {
-		System.out.println("E");
-	}
+	
 
 
 }
+
