fcb0a6cc744f5d5601067dacf7550d6ae960478e
DineshBabu32 <postbox0032@gmail.com>
2025-02-22 21:06:38 +0530

    dinesh push

---
+++ src/test/java/org/git/Sample.java
@@ -0,0 +1,19 @@
+package org.git;
+
+public class Sample {
+	
+
+	public void methodA() {
+		System.out.println("A");
+	}
+
+	public void methodB() {
+		System.out.println("B");
+	}
+
+	public void methodC() {
+		System.out.println("C");
+	}
+
+
+}
